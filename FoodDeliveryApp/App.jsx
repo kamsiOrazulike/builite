ded76006d74c22a822b5cd1667f8071c4b76ce49
kamsiOrazulike <kamsi_orazulike@icloud.com>
2023-01-17 20:37:24 +0100

    welcome edits

--- FoodDeliveryApp/App.jsx
+++ FoodDeliveryApp/App.jsx
@@ -1,21 +1,51 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    display: 'flex',
+    position: 'relative',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    backgroundColor: '#1B1E1C',
+  },
+
+  container: {
+    top: '20%',
+    left: '5%',
+    display: 'flex',
+    position: 'relative',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    // backgroundColor: '#FFFFFF50',
+  },
+
+  h1: {
+    fontSize: 36,
+    fontWeight: 800,
+    color: '#FFFFFF',
+    // backgroundColor: '#00000050',
+  },
+
+  h2: {
+    fontSize: 24,
+    fontWeight: 300,
+    color: '#FFFFFF',
+    // backgroundColor: '#00000050',
+  },
+});
 
 const App = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text
-        style={{
-          fontSize: 30,
-        }}>
-        Hello, there!
-      </Text>
-    </View>
+    <>
+      <View style={styles.wrapper}>
+        <View style={styles.container}>
+          <Text style={styles.h1}>Hey there!</Text>
+          <Text style={styles.h2}>Welcome to my first app</Text>
+        </View>
+      </View>
+    </>
   );
 };
 export default App;
